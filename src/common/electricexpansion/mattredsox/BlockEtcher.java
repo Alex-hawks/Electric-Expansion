736d068d965fa90ff42aeee29901eb1dd6b346c1
Matt RedSox <mattredsox99@gmail.com>
2012-10-01 20:25:39 -0700

    I am a real derp. Fixing huge mistake

---
+++ src/common/electricexpansion/mattredsox/BlockEtcher.java
@@ -0,0 +1,51 @@
+package electricexpansion.mattredsox;
+
+import electricexpansion.ElectricExpansion;
+import net.minecraft.src.Block;
+import net.minecraft.src.BlockContainer;
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.Material;
+import net.minecraft.src.TileEntity;
+import net.minecraft.src.World;
+
+public class BlockEtcher extends BlockContainer
+{
+public BlockEtcher(int par1)
+{
+         super(par1, Material.wood);
+         this.setCreativeTab(CreativeTabs.tabDecorations);
+}
+/**
+         * Returns the block texture based on the side being looked at. Args: side
+public int getBlockTextureFromSide(int side)
+{
+         switch(side)
+         {
+         case 0: return mod_LegoCrafting.LegoBottom;
+         case 1: return mod_LegoCrafting.LegoTop;
+         case 2:
+         case 3:
+         case 4:
+         case 5: return mod_LegoCrafting.LegoSide;
+         default : return 0;
+         }
+
+         */
+@Override
+public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+{
+  if (!par1World.isRemote)
+  {
+      par5EntityPlayer.openGui(ElectricExpansion.instance, 2, par1World, par2, par3, par4);
+      return true;
+  }
+
+  return true;
+}
+@Override
+public TileEntity createNewTileEntity(World var1) {
+	return new TileEntityEtcher();
+}
+
+}
---
+++ src/minecraft/electricexpansion/client/mattredsox/GuiVoltDetector.java
@@ -0,0 +1,72 @@
+package electricexpansion.client.mattredsox;
+
+import net.minecraft.src.*;
+import net.minecraftforge.common.ForgeDirection;
+
+
+import org.lwjgl.opengl.GL11;
+
+import electricexpansion.mattredsox.TileEntityVoltDetector;
+
+
+import universalelectricity.UniversalElectricity;
+import universalelectricity.electricity.ElectricInfo;
+import universalelectricity.electricity.ElectricInfo.ElectricUnit;
+
+public class GuiVoltDetector extends GuiScreen {
+
+	private TileEntityVoltDetector voltDetect;
+	private ForgeDirection dir;
+
+	protected int xSize = 176;
+
+	protected int ySize = 166;
+
+	public GuiVoltDetector(InventoryPlayer inventory, TileEntityVoltDetector voltDetect) {
+		super();
+		this.voltDetect = voltDetect;
+	}
+
+	public boolean doesGuiPauseGame() {
+		return false;
+	}
+
+	protected void drawBackgroundLayer(float par1, int par2, int par3) {
+		int var4 = this.mc.renderEngine.getTexture("/electricexpansion/VoltDetectorGUI.png");
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.mc.renderEngine.bindTexture(var4);
+		int var5 = (this.width - this.xSize) / 2;
+		int var6 = (this.height - this.ySize) / 2;
+		this.drawTexturedModalRect(var5, var6, 0, 0, this.xSize, this.ySize);
+	}
+	protected void drawForegroundLayer() {
+		this.drawCenteredString(mc.fontRenderer, ElectricInfo.getDisplay(voltDetect.voltsin, ElectricUnit.VOLTAGE), (this.width - (this.xSize - 180)) / 2, (this.height - (this.ySize - 80)) / 2, 0xFFFFFF);
+
+		mc.fontRenderer.drawString("Volt Detector", (this.width - (this.xSize - 110)) / 2, (this.height - (this.ySize - 20)) / 2, 0x333333);
+	}
+
+	public void drawScreen(int par1, int par2, float par3) {
+		this.drawDefaultBackground();
+		this.drawBackgroundLayer(par3, par1, par2);
+		this.drawForegroundLayer();
+		super.drawScreen(par1, par2, par3);
+	}
+
+	public void initGui() {
+		super.initGui();
+	}
+
+	protected void keyTyped(char par1, int par2) {
+		if (par2 == 1 || par2 == this.mc.gameSettings.keyBindInventory.keyCode) {
+			this.mc.thePlayer.closeScreen();
+		}
+	}
+
+	public void updateScreen() {
+		super.updateScreen();
+
+		if (!this.mc.thePlayer.isEntityAlive() || this.mc.thePlayer.isDead) {
+			this.mc.thePlayer.closeScreen();
+		}
+	}
+}
