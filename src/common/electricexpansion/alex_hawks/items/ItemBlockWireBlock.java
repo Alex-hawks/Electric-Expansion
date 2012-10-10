f04afac4d0ca6721d8e722c6c6a1f2c726cc6611
Alex <alex_hawks@hotmail.com>
2012-10-10 21:09:16 +0800

    Re-adding accidentally deleted files
    I hate git bash...

---
+++ src/common/electricexpansion/alex_hawks/items/ItemBlockWireBlock.java
@@ -0,0 +1,38 @@
+package electricexpansion.alex_hawks.items;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.ItemBlock;
+import net.minecraft.src.ItemStack;
+
+public class ItemBlockWireBlock extends ItemBlock 
+{
+	public ItemBlockWireBlock(int par1, Block mainBlock)
+	{
+	super(par1);
+	setHasSubtypes(true);
+	}
+	public int getMetadata(int par1)
+    {
+        return par1;
+    }
+	public String getItemNameIS(ItemStack i) 
+	{
+		String name = null;
+		int j = i.getItemDamage();
+		if (j != -1)
+		{
+			if(j == 0)
+				name = "Copper";
+			else if(j == 1)
+				name = "Tin"; 
+			else if(j == 2)
+				name = "Silver";
+			else if(j == 3)
+				name = "HV";
+			//expandable
+			else name = "Unknown";
+		}
+		return i.getItem().getItemName() + "." + name;
+	}
+	
+}
