f3c71a3f26851dc076261a472cef435b53f1d583
Alex <alex_hawks@hotmail.com>
2012-10-11 23:15:35 +0800

    Forgot some stuff with the last commit

--- src/common/electricexpansion/alex_hawks/misc/ContainerWireMill.java
+++ src/common/electricexpansion/alex_hawks/misc/ContainerWireMill.java
@@ -13,87 +13,117 @@ import universalelectricity.implement.IItemElectric;
 
 public class ContainerWireMill extends Container
 {
-	private TileEntityWireMill tileEntity;
+    private TileEntityWireMill tileEntity;
 
-	public ContainerWireMill(InventoryPlayer par1InventoryPlayer, TileEntityWireMill tileEntity)
-	{
-		this.tileEntity = tileEntity;
-		this.addSlotToContainer(new SlotElectricItem(tileEntity, 0, 55, 49)); //Electric Input Slot
-		this.addSlotToContainer(new SlotFurnace(par1InventoryPlayer.player, tileEntity, 1, 108, 25)); //Smelting result
-		int var3;
+    public ContainerWireMill(InventoryPlayer par1InventoryPlayer, TileEntityWireMill tileEntity)
+    {
+        this.tileEntity = tileEntity;
+        this.addSlotToContainer(new SlotElectricItem(tileEntity, 0, 55, 49)); //Electric Input Slot
+        this.addSlotToContainer(new Slot(tileEntity, 1, 55, 25)); //To be smelted
+        this.addSlotToContainer(new SlotFurnace(par1InventoryPlayer.player, tileEntity, 2, 108, 25)); //Smelting result
+        int var3;
 
-		for (int i=0; i < 3; i++) //To be smelted
-			this.addSlotToContainer(new Slot(tileEntity, i+2, 37 + i*18, 25));
-		for (int i=0; i < 3; i++) //To be smelted
-			this.addSlotToContainer(new Slot(tileEntity, i+5, 37 + i*18, 43));
-		
-		for (var3 = 0; var3 < 3; ++var3)
-			for (int var4 = 0; var4 < 9; ++var4)
-				this.addSlotToContainer(new Slot(par1InventoryPlayer, var4 + var3 * 9 + 9, 8 + var4 * 18, 84 + var3 * 18));
+        for (var3 = 0; var3 < 3; ++var3)
+        {
+            for (int var4 = 0; var4 < 9; ++var4)
+            {
+                this.addSlotToContainer(new Slot(par1InventoryPlayer, var4 + var3 * 9 + 9, 8 + var4 * 18, 84 + var3 * 18));
+            }
+        }
 
-		for (var3 = 0; var3 < 9; ++var3)
-			this.addSlotToContainer(new Slot(par1InventoryPlayer, var3, 8 + var3 * 18, 142));
-
-		tileEntity.openChest();
-	}
-
-	public void onCraftGuiClosed(EntityPlayer entityplayer)
-	{
+        for (var3 = 0; var3 < 9; ++var3)
+        {
+            this.addSlotToContainer(new Slot(par1InventoryPlayer, var3, 8 + var3 * 18, 142));
+        }
+        
+        tileEntity.openChest();
+    }
+    
+    public void onCraftGuiClosed(EntityPlayer entityplayer)
+    {
 		super.onCraftGuiClosed(entityplayer);
 		tileEntity.closeChest();
-	}
+    }
 
-	@Override
-	public boolean canInteractWith(EntityPlayer par1EntityPlayer)
-	{
-		return this.tileEntity.isUseableByPlayer(par1EntityPlayer);
-	}
+    @Override
+    public boolean canInteractWith(EntityPlayer par1EntityPlayer)
+    {
+        return this.tileEntity.isUseableByPlayer(par1EntityPlayer);
+    }
 
-	/**
-	 * Called to transfer a stack from one inventory to the other eg. when shift clicking.
-	 */
-	@Override
-	public ItemStack transferStackInSlot(int par1)
-	{
-		ItemStack var2 = null;
-		Slot var3 = (Slot)this.inventorySlots.get(par1);
+    /**
+     * Called to transfer a stack from one inventory to the other eg. when shift clicking.
+     */
+    @Override
+    public ItemStack transferStackInSlot(int par1)
+    {
+        ItemStack var2 = null;
+        Slot var3 = (Slot)this.inventorySlots.get(par1);
 
-		if (var3 != null && var3.getHasStack())
-		{
-			ItemStack var4 = var3.getStack();
-			var2 = var4.copy();
+        if (var3 != null && var3.getHasStack())
+        {
+            ItemStack var4 = var3.getStack();
+            var2 = var4.copy();
 
-			if (par1 == 2)
-			{
-				if (!this.mergeItemStack(var4, 3, 39, true))
-					return null;
-				var3.onSlotChange(var4, var2);
-			}
-			else if (par1 != 1 && par1 != 0)
-			{
-				if (var4.getItem() instanceof IItemElectric)
-					if (!this.mergeItemStack(var4, 0, 1, false))
-						return null;
-				else if (FurnaceRecipes.smelting().getSmeltingResult(var4) != null)
-					if (!this.mergeItemStack(var4, 1, 2, false))
-						return null;
-				else if (par1 >= 3 && par1 < 30)
-					if (!this.mergeItemStack(var4, 30, 39, false))
-						return null;
-				else if (par1 >= 30 && par1 < 39 && !this.mergeItemStack(var4, 3, 30, false))
-					return null;
-			else if (!this.mergeItemStack(var4, 3, 39, false))
-				return null;
+            if (par1 == 2)
+            {
+                if (!this.mergeItemStack(var4, 3, 39, true))
+                {
+                    return null;
+                }
 
-			if (var4.stackSize == 0)
-				var3.putStack((ItemStack)null);
-			else
-				var3.onSlotChanged();
-			if (var4.stackSize == var2.stackSize)
-				return null;
-			}
-			var3.onPickupFromSlot(var4);
-		}
-		return var2;
-	}
+                var3.onSlotChange(var4, var2);
+            }
+            else if (par1 != 1 && par1 != 0)
+            {
+                if (var4.getItem() instanceof IItemElectric)
+                {
+                    if (!this.mergeItemStack(var4, 0, 1, false))
+                    {
+                        return null;
+                    }
+                }
+                else if (FurnaceRecipes.smelting().getSmeltingResult(var4) != null)
+                {
+                    if (!this.mergeItemStack(var4, 1, 2, false))
+                    {
+                        return null;
+                    }
+                }
+                else if (par1 >= 3 && par1 < 30)
+                {
+                    if (!this.mergeItemStack(var4, 30, 39, false))
+                    {
+                        return null;
+                    }
+                }
+                else if (par1 >= 30 && par1 < 39 && !this.mergeItemStack(var4, 3, 30, false))
+                {
+                    return null;
+                }
+            }
+            else if (!this.mergeItemStack(var4, 3, 39, false))
+            {
+                return null;
+            }
+
+            if (var4.stackSize == 0)
+            {
+                var3.putStack((ItemStack)null);
+            }
+            else
+            {
+                var3.onSlotChanged();
+            }
+
+            if (var4.stackSize == var2.stackSize)
+            {
+                return null;
+            }
+
+            var3.onPickupFromSlot(var4);
+        }
+
+        return var2;
+    }
 }
