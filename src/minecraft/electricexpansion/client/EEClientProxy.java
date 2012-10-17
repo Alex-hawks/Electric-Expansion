e854c990470e9853eeb9a30d1db94da6840e2ad0
Matt RedSox <mattredsox99@gmail.com>
2012-10-17 07:54:00 -0700

    Fixed NBT Issues - Matt
    Note to self: Register Tile Entities in Client Proxy too...

--- src/minecraft/electricexpansion/client/EEClientProxy.java
+++ src/minecraft/electricexpansion/client/EEClientProxy.java
@@ -7,6 +7,7 @@ import net.minecraftforge.client.MinecraftForgeClient;
 import universalelectricity.basiccomponents.RenderCopperWire;
 import universalelectricity.basiccomponents.TileEntityCopperWire;
 import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
 import electricexpansion.alex_hawks.cables.TileEntityInsulatedWire;
 import electricexpansion.alex_hawks.cables.TileEntityRawWire;
 import electricexpansion.alex_hawks.cables.TileEntityRedstoneWire;
@@ -14,6 +15,11 @@ import electricexpansion.alex_hawks.cables.TileEntitySwitchWire;
 import electricexpansion.alex_hawks.cables.TileEntitySwitchWireOff;
 import electricexpansion.client.alex_hawks.RenderInsulatedWire;
 import electricexpansion.client.alex_hawks.RenderRawWire;
+import electricexpansion.mattredsox.tileentities.TileEntityAdvBatteryBox;
+import electricexpansion.mattredsox.tileentities.TileEntityDOWNTransformer;
+import electricexpansion.mattredsox.tileentities.TileEntityFuse;
+import electricexpansion.mattredsox.tileentities.TileEntityUPTransformer;
+import electricexpansion.mattredsox.tileentities.TileEntityVoltDetector;
 
 public class EEClientProxy extends electricexpansion.EECommonProxy
 {
@@ -28,12 +34,20 @@ public class EEClientProxy extends electricexpansion.EECommonProxy
 	@Override
 	public void init()
 	{
+		//Mattredsox's Tile entity registrations
+			GameRegistry.registerTileEntity(TileEntityAdvBatteryBox.class, "TileEntityAdvBox");
+			GameRegistry.registerTileEntity(TileEntityUPTransformer.class, "TileEntityUpTrans");
+			GameRegistry.registerTileEntity(TileEntityVoltDetector.class, "TileEntityVoltDet");
+			GameRegistry.registerTileEntity(TileEntityDOWNTransformer.class, "TileEntityDownTrans");
+			GameRegistry.registerTileEntity(TileEntityFuse.class, "TileEntityFuse");
+		
 			//Alex's Tile Entity Renderer registrations
 			ClientRegistry.registerTileEntity(TileEntityRawWire.class, "TileEntityRawWire", new RenderRawWire());
 			ClientRegistry.registerTileEntity(TileEntityInsulatedWire.class, "TileEntityInsulatedWire", new RenderInsulatedWire());
 			ClientRegistry.registerTileEntity(TileEntityRedstoneWire.class, "TileEntityRedstoneWire", new RenderInsulatedWire());
 			ClientRegistry.registerTileEntity(TileEntitySwitchWire.class, "TileEntitySwitchWire", new RenderInsulatedWire());
 			ClientRegistry.registerTileEntity(TileEntitySwitchWireOff.class, "TileEntitySwitchWireOff", new RenderInsulatedWire());	
+			
 	}
 
 }
