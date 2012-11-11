dd3be83b9a7a83d0344c0a21ead475f1e2a7c6fc
Alex_hawks <alex_hawks@hotmail.com>
2012-11-11 23:02:18 +0800

    Removed some redundancies that I forgot about...

--- src/common/electricexpansion/alex_hawks/helpers/TileEntityCableHelper.java
+++ src/common/electricexpansion/alex_hawks/helpers/TileEntityCableHelper.java
@@ -20,10 +20,10 @@ import electricexpansion.api.CableInterfaces.ISelectiveConnector;
 public abstract class TileEntityCableHelper extends TileEntityConductor implements ISelectiveConnector
 {
 	@Override
-    public Packet getDescriptionPacket()
-    {
-        return universalelectricity.prefab.network.PacketManager.getPacket("ElecEx", this);
-    }
+	public Packet getDescriptionPacket()
+	{
+		return universalelectricity.prefab.network.PacketManager.getPacket("ElecEx", this);
+	}
 	@Override
 	public boolean canConnect(ForgeDirection side)
 	{
@@ -33,20 +33,20 @@ public abstract class TileEntityCableHelper extends TileEntityConductor implemen
 	{
 		boolean returnValue = false;
 		int x=0,y=0,z=0;
-				if	(side == 0){x=0;y=1;z=0;}
+		if	(side == 0){x=0;y=1;z=0;}
 		else 	if	(side == 1){x=0;y=-1;z=0;}
 		else 	if	(side == 2){x=0;y=0;z=-1;}
 		else 	if	(side == 3){x=0;y=0;z=1;}
 		else 	if	(side == 4){x=-1;y=0;z=0;}
 		else 	if	(side == 5){x=1;y=0;z=0;}
-		
+
 		int thisID 			= this.worldObj.getBlockId(this.xCoord, this.yCoord, this.zCoord);
 		int thismeta 		= this.worldObj.getBlockMetadata(this.xCoord, this.yCoord, this.zCoord);
 		TileEntity thisTE 	= this.worldObj.getBlockTileEntity(this.xCoord, this.yCoord, this.zCoord);
 		int ID 				= this.worldObj.getBlockId(this.xCoord + x, this.yCoord + y, this.zCoord + z);
 		int meta 			= this.worldObj.getBlockMetadata(this.xCoord + x, this.yCoord + y, this.zCoord + z);
 		TileEntity TE 		= this.worldObj.getBlockTileEntity(this.xCoord + x, this.yCoord + y, this.zCoord + z);
-		
+
 		if((TE instanceof IConnector) && !(TE instanceof IConductor))
 			returnValue = true;
 		else if(TE instanceof ISelectiveConnector)
@@ -66,27 +66,24 @@ public abstract class TileEntityCableHelper extends TileEntityConductor implemen
 	public String cableType(int ID, int meta)
 	{
 		String type = "Unknown";
-	//	if(ID == BasicComponents.blockCopperWire.blockID)
-		//	type = "Copper";
-		 switch(meta)
+		switch(meta)
 		{
-			case 0: type = "Copper";
-					break;
-			case 1: type = "Tin";
-					break;
-			case 2: type = "Silver";
-					break;
-			case 3: type = "Aluminium";
-					break;
-			case 4: type = "Endium";
-					break;
-			case 5: type = "Connector";
-					break;
+		case 0: type = "Copper";
+		break;
+		case 1: type = "Tin";
+		break;
+		case 2: type = "Silver";
+		break;
+		case 3: type = "Aluminium";
+		break;
+		case 4: type = "Endium";
+		break;
+		case 5: type = "Connector";
+		break;
 		}
 		return type;
-		
 	}
-	
+
 	@Override
 	public void onOverCharge() 
 	{
@@ -104,7 +101,7 @@ public abstract class TileEntityCableHelper extends TileEntityConductor implemen
 				setToID = 0;
 			if(ID == ElectricExpansion.SwitchWireBlock)
 				setToID = Block.stone.blockID;
-		
+
 			this.worldObj.setBlockWithNotify(this.xCoord, this.yCoord, this.zCoord, setToID);
 		}
 	}
