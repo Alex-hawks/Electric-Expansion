ea8083f82ba5030b18cb28c90bbc2b63b19c029f
Alex_hawks <alex_hawks@hotmail.com>
2012-11-20 22:02:00 +0800

    Should Fix some stuff :D

--- src/common/electricexpansion/alex_hawks/blocks/BlockWireMill.java
+++ src/common/electricexpansion/alex_hawks/blocks/BlockWireMill.java
@@ -1,28 +1,40 @@
 package electricexpansion.alex_hawks.blocks;
 
-import net.minecraft.src.BlockContainer;
+import universalelectricity.prefab.BlockMachine;
 import net.minecraft.src.CreativeTabs;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.Material;
 import net.minecraft.src.TileEntity;
 import net.minecraft.src.World;
+import electricexpansion.EECommonProxy;
 import electricexpansion.ElectricExpansion;
 import electricexpansion.alex_hawks.machines.TileEntityWireMill;
 
-public class BlockWireMill extends BlockContainer
+public class BlockWireMill extends BlockMachine
 {
+	private int orientation;
 	public BlockWireMill(int par1)
 	{
-		super(par1, Material.iron);
+		super("WireMill", par1, Material.iron);
 		this.setCreativeTab(CreativeTabs.tabDecorations);
+		this.setTextureFile(EECommonProxy.ABLOCK);
+	}
+	
+	@Override
+	public boolean onUseWrench(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer)
+	{
+		if(this.orientation == 4)
+			this.orientation = 0;
+		this.orientation++;
+		return true;
 	}
 
 	@Override
-	public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+	public boolean onMachineActivated(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer)
 	{
 		if (!par1World.isRemote)
 		{
-			par5EntityPlayer.openGui(ElectricExpansion.instance, 2, par1World, par2, par3, par4);
+			par5EntityPlayer.openGui(ElectricExpansion.instance, 2, par1World, x, y, z);
 			return true;
 		}
 
@@ -31,6 +43,14 @@ public class BlockWireMill extends BlockContainer
 	@Override
 	public TileEntity createNewTileEntity(World var1) {
 		return new TileEntityWireMill();
+	}
+	
+	@Override
+	public int getBlockTextureFromSide(int side)
+	{
+		if(side == 1)
+			return 253;
+		return 254;	
 	}
 
 }
