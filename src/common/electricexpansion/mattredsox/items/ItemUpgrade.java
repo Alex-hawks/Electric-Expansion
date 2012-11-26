a637e9aa1d7c164f26b6306c29fd999ac4b436a6
Alex <alex_hawks@hotmail.com>
2012-11-26 15:16:53 +0800

    This might help @Mattredsox

--- src/common/electricexpansion/mattredsox/items/ItemUpgrade.java
+++ src/common/electricexpansion/mattredsox/items/ItemUpgrade.java
@@ -13,78 +13,78 @@ import net.minecraft.src.ItemStack;
 
 public class ItemUpgrade extends Item implements IModifier
 {
-    private String[] names = new String[] {"Tier 1 Storage Upgrade", "Tier 2 Storage Upgrade", "Tier 3 Storage Upgrade", "BC Compatibility Upgrade", "IC2 Compatibility Upgrade"};
+	private String[] names = new String[] {"Tier 1 Storage Upgrade", "Tier 2 Storage Upgrade", "Tier 3 Storage Upgrade", "BC Compatibility Upgrade", "IC2 Compatibility Upgrade"};
 
-    public ItemUpgrade(int id, int texture)
-    {
-        super(id);
-        this.setMaxDamage(0);
-        this.setMaxStackSize(1);
-        this.setHasSubtypes(true);
-        this.setCreativeTab(UETab.INSTANCE);
-    }
+	public ItemUpgrade(int id, int texture)
+	{
+		super(id);
+		this.setMaxDamage(0);
+		this.setMaxStackSize(1);
+		this.setHasSubtypes(true);
+		this.setCreativeTab(UETab.INSTANCE);
+	}
 
-    @Override
-    public int getMetadata(int damage)
-    {
-        return damage;
-    }
+	@Override
+	public int getMetadata(int damage)
+	{
+		return damage;
+	}
 
-    @Override
-    public String getItemNameIS(ItemStack itemstack)
-    {
-        return names[itemstack.getItemDamage()];
-    }
+	@Override
+	public String getItemNameIS(ItemStack itemstack)
+	{
+		return names[itemstack.getItemDamage()];
+	}
 
-    @Override
-    public int getIconFromDamage(int i)
-    {
-if(i == 0)
+	@Override
+	public int getIconFromDamage(int i)
+	{
+		if(i == 0)
 
-	{return 5;}
-if(i == 1)
-	{return 4;}
-if(i == 2)
-	{return 8;}
-if(i == 3)
-	{ return 7;}
-	return 6;
-	
-    }
-    
+		{return 5;}
+		if(i == 1)
+		{return 4;}
+		if(i == 2)
+		{return 8;}
+		if(i == 3)
+		{ return 7;}
+		return 6;
+
+	}
+
 	@Override
 	public String getTextureFile()
 	{return EECommonProxy.MattItem_TEXTURE_FILE;}
 
-    @Override
-    public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List)
-    {
-    	for(int i = 0; i < names.length; i++)
-        {
-    		par3List.add(new ItemStack(this, 1, i));
-        }
-    }
+	@Override
+	public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List)
+	{
+		for(int i = 0; i < names.length; i++)
+		{
+			par3List.add(new ItemStack(this, 1, i));
+		}
+	}
 
 	@Override
 	public String getName(ItemStack itemstack) {
-	if(itemstack.getItemDamage() == 0) {
-		return "Capacity"; }
-	if(itemstack.getItemDamage() == 1) {
-		return "Capacity";	}
-	if(itemstack.getItemDamage() == 2) {
+		if(itemstack.getItemDamage() == 0) {
 			return "Capacity"; }
-	
-	return null;
+		if(itemstack.getItemDamage() == 1) {
+			return "Capacity";	}
+		if(itemstack.getItemDamage() == 2) {
+			return "Capacity"; }
+
+		return null;
 	}
 
 	@Override
 	public int getEffectiveness(ItemStack itemstack) {
-	if(itemstack.getItemDamage() == 0) {
-			return 6000000; }
+		if(itemstack.getItemDamage() == 0) {
+			return 1000000; }
 		if(itemstack.getItemDamage() == 1) {
-			return 9000000;	}
+			return 2000000;	}
 		if(itemstack.getItemDamage() == 2) {
-				return 12000000; }
-		
+			return 3000000; }
+
 		return 0;	}
 }
