25c30cb670efa4a6ababea5593441f365c602f81
Alex <alex_hawks@hotmail.com>
2012-12-05 12:53:14 +0800

    change electrocution to seconds, from ticks

--- src/common/electricexpansion/alex_hawks/cables/TileEntityRawWire.java
+++ src/common/electricexpansion/alex_hawks/cables/TileEntityRawWire.java
@@ -6,50 +6,59 @@ import electricexpansion.alex_hawks.helpers.TileEntityCableHelper;
 
 public class TileEntityRawWire extends TileEntityCableHelper
 {
-    @Override
+	private byte ticks = 0;
+	
+	@Override
 	public double getResistance() 
-    //Values will NOT be actual values or precise relative values. But if x is meant to be greater than y, it will be. 
-    //Maybe by 10^10 or 10^-10. But the one meant to be greater, will be.
+	//Values will NOT be actual values or precise relative values. But if x is meant to be greater than y, it will be. 
+	//Maybe by 10^10 or 10^-10. But the one meant to be greater, will be.
 	{
 		int meta = worldObj.getBlockMetadata(xCoord, yCoord, zCoord);
-    	switch(meta)
-    	{
-    	case 0: return 0.10; 
-    	case 1: return 0.08;
-    	case 2: return 0.04;
-    	case 3: return 0.40;
-    	case 5: return 0.01;
-    	default: return 0.10;
-    	}
+		switch(meta)
+		{
+		case 0: return 0.10; 
+		case 1: return 0.08;
+		case 2: return 0.04;
+		case 3: return 0.40;
+		case 5: return 0.01;
+		default: return 0.10;
+		}
 	}
-    
-    @Override
-    public boolean canUpdate()
-    {return true;}
-    
-    @Override
-    public void updateEntity()
-    {this.damageEntity();}
-    
-    public void damageEntity()
-    {
-    	if(!worldObj.isRemote)
-    	{
-    		if(this.getNetwork().getProduced().getWatts() > 0)
-    		{
-    			for(Object player : this.worldObj.playerEntities)
-    			{
-    				if(player instanceof EntityPlayer)
-    				{
-    					if(((EntityPlayer)player).getPlayerCoordinates().getDistanceSquared(this.xCoord, this.yCoord, this.zCoord) <= 9)
-    						((EntityPlayer)player).attackEntityFrom(UEDamageSource.electrocution, this.getDamageFromMeta(this.getBlockMetadata()));
-    				}
-    			}
-    		}
-    	}
-    }
-	
-    private int getDamageFromMeta(int meta) 
+
+	@Override
+	public boolean canUpdate()
+	{return true;}
+
+	@Override
+	public void updateEntity()
+	{
+		this.ticks++;
+		if(this.ticks == 20)
+		{
+			this.damagePlayers();
+			this.ticks = 0;
+		}
+	}
+
+	public void damagePlayers()
+	{
+		if(!worldObj.isRemote)
+		{
+			if(this.getNetwork().getProduced().getWatts() > 0)
+			{
+				for(Object player : this.worldObj.playerEntities)
+				{
+					if(player instanceof EntityPlayer)
+					{
+						if(((EntityPlayer)player).getPlayerCoordinates().getDistanceSquared(this.xCoord, this.yCoord, this.zCoord) <= 9)
+							((EntityPlayer)player).attackEntityFrom(UEDamageSource.electrocution, this.getDamageFromMeta(this.getBlockMetadata()));
+					}
+				}
+			}
+		}
+	}
+
+	private int getDamageFromMeta(int meta) 
 	{
 		switch(meta)
 		{
