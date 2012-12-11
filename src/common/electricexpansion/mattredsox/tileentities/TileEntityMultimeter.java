2d8766963d92a385397fc55427f9fbd1ea62a285
Henry Mao <admin@calclavia.com>
2012-12-11 19:11:45 +0800

    Fix Multimeter
    Edited this on the fly on Github, never tested.

--- src/common/electricexpansion/mattredsox/tileentities/TileEntityMultimeter.java
+++ src/common/electricexpansion/mattredsox/tileentities/TileEntityMultimeter.java
@@ -24,36 +24,30 @@ import com.google.common.io.ByteArrayDataInput;
 import electricexpansion.ElectricExpansion;
 import electricexpansion.mattredsox.blocks.BlockMultimeter;
 
-public class TileEntityMultimeter extends TileEntityElectricityReceiver implements IJouleStorage, IPacketReceiver
+public class TileEntityMultimeter extends TileEntityElectricityReceiver implements IPacketReceiver
 {
-	private double joules = 0;
-	
-	private boolean isFull = false;
-
 	private int playersUsing = 0;
 
-	public ElectricityPack elecPack = new ElectricityPack(0, 0);
-
-
-	public TileEntityMultimeter()
-	{
-		super();
-	}
+	/**
+	 * The reading this multimeter did on how much electricity was produced on the last tick.
+	*/
+	public final ElectricityPack electricityReading = new ElectricityPack();
 
 	@Override
 	public void initiate()
 	{
-		ElectricityConnections.registerConnector(this, EnumSet.of(ForgeDirection.getOrientation(this.getBlockMetadata() - BlockMultimeter.MULTIMETER_METADATA + 2), ForgeDirection.getOrientation(this.getBlockMetadata() - BlockMultimeter.MULTIMETER_METADATA + 2).getOpposite()));
+		//The connection should be only one side, since that's all you need to test and get a reading.
+		ElectricityConnections.registerConnector(this, EnumSet.of(ForgeDirection.getOrientation(this.getBlockMetadata() - BlockMultimeter.MULTIMETER_METADATA + 2)));
 	}
 	
 	@Override
 	public void updateEntity()
 	{
 		super.updateEntity();
-
-		if (!this.isDisabled())
+		
+		if (!this.worldObj.isRemote)
 		{
-			if (!this.worldObj.isRemote)
+			if (!this.isDisabled())
 			{
 				ForgeDirection inputDirection = ForgeDirection.getOrientation(this.getBlockMetadata() - BlockMultimeter.MULTIMETER_METADATA + 2).getOpposite();
 				TileEntity inputTile = Vector3.getTileEntityFromSide(this.worldObj, Vector3.get(this), inputDirection);
@@ -62,59 +56,25 @@ public class TileEntityMultimeter extends TileEntityElectricityReceiver implemen
 				{
 					if (inputTile instanceof IConductor)
 					{
-					 elecPack = ((IConductor)inputTile).getNetwork().getProduced();
-
-						if (this.joules >= this.getMaxJoules())
-						{
-							((IConductor) inputTile).getNetwork().stopRequesting(this);
-						}
-						else
-						{
-							((IConductor) inputTile).getNetwork().startRequesting(this, this.getMaxJoules() - this.getJoules(), this.getVoltage());
-							this.setJoules(this.joules + ((IConductor) inputTile).getNetwork().consumeElectricity(this).getWatts());
-						}
+					 	this.electricityReading = ((IConductor)inputTile).getNetwork().getProduced();
 					}
-				}
-			}
-			/**
-			 * Output Electricity
-			 */
-			if (this.joules > 0)
-			{
-				ForgeDirection outputDirection = ForgeDirection.getOrientation(this.getBlockMetadata() - BlockMultimeter.MULTIMETER_METADATA + 2);
-				TileEntity tileEntity = Vector3.getTileEntityFromSide(this.worldObj, Vector3.get(this), outputDirection);
-
-				if (tileEntity != null)
-				{
-					TileEntity connector = Vector3.getConnectorFromSide(this.worldObj, Vector3.get(this), outputDirection);
-
-					// Output UE electricity
-					if (connector instanceof IConductor)
+					else
 					{
-						double joulesNeeded = ((IConductor) connector).getNetwork().getRequest().getWatts();
-						double transferAmps = Math.max(Math.min(Math.min(ElectricInfo.getAmps(joulesNeeded, this.elecPack.voltage), ElectricInfo.getAmps(this.joules, this.elecPack.voltage)), 80), 0);
-
-						if (!this.worldObj.isRemote && transferAmps > 0)
-						{
-							((IConductor) connector).getNetwork().startProducing(this, transferAmps, elecPack.voltage);
-							this.setJoules(this.joules - ElectricInfo.getWatts(transferAmps, elecPack.voltage));
-						}
-						else
-						{
-							((IConductor) connector).getNetwork().stopProducing(this);
-						}
+						this.electricityReading.amperes = 0;
+						this.electricityReading.voltage = 0;
 					}
 				}
+				else
+				{
+					this.electricityReading.amperes = 0;
+					this.electricityReading.voltage = 0;
+				}
+			
 			}
-			this.setJoules(this.joules - 1);
-
-		}
-
-		if (!this.worldObj.isRemote)
-		{
-			if (this.ticks % 3 == 0 && this.playersUsing > 0)
+		
+			if (this.ticks % 10 == 0 && this.playersUsing > 0)
 			{
-				PacketManager.sendPacketToClients(getDescriptionPacket(), this.worldObj, Vector3.get(this), 12);
+				PacketManager.sendPacketToClients(this.getDescriptionPacket(), this.worldObj, Vector3.get(this), 12);
 			}
 		}
 	}
@@ -122,81 +82,46 @@ public class TileEntityMultimeter extends TileEntityElectricityReceiver implemen
 	@Override
 	public Packet getDescriptionPacket()
 	{
-		return PacketManager.getPacket(ElectricExpansion.CHANNEL, this, (int) 1, this.elecPack.amperes, this.elecPack.voltage);
+		return PacketManager.getPacket(ElectricExpansion.CHANNEL, this, (int) 1, this.electricityReading.amperes, this.electricityReading.voltage);
 
 	}
 
 	@Override
 	public void handlePacketData(INetworkManager network, int type, Packet250CustomPayload packet, EntityPlayer player, ByteArrayDataInput dataStream)
 	{
-		try
+		if(this.worldObj.isRemote)
 		{
-			int id = dataStream.readInt();
-			if(id == -1) 
+			try
 			{
-				if(dataStream.readBoolean())
+				final int id = dataStream.readInt();
+				
+				if(id == -1) 
 				{
-					this.playersUsing++;
+					if(dataStream.readBoolean())
+					{
+						this.playersUsing++;
+					}
+					else
+					{
+						playersUsing--;
+					}
 				}
-			else playersUsing--;
+				else
+				{
+					this.electricityReading.amperes = dataStream.readDouble();			
+					this.electricityReading.voltage = dataStream.readDouble();
+				}
+				
 			}
-			if(id != -1)
+			catch (Exception e)
 			{
-			this.elecPack.amperes = dataStream.readDouble();			
-			this.elecPack.voltage = dataStream.readDouble();
+				e.printStackTrace();
 			}
-			
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
 		}
 	}
-	/**
-	 * Reads a tile entity from NBT.
-	 */
-	@Override
-	public void readFromNBT(NBTTagCompound par1NBTTagCompound)
-	{
-		super.readFromNBT(par1NBTTagCompound);
-		this.joules = par1NBTTagCompound.getDouble("electricityStored");
-	}
-	
 
-	/**
-	 * Writes a tile entity to NBT.
-	 */
-	@Override
-	public void writeToNBT(NBTTagCompound par1NBTTagCompound)
-	{
-		super.writeToNBT(par1NBTTagCompound);
-		par1NBTTagCompound.setDouble("electricityStored", this.joules);
-	}
-
-	
 	public String getInvName()
 	{
 		return "Multimeter";
 	}
-
-
-	@Override
-	public double getJoules(Object... data)
-	{
-		return this.joules;
-	}
-
-	@Override
-	public void setJoules(double joules, Object... data)
-	{
-		this.joules = Math.max(Math.min(joules, this.getMaxJoules()), 0);
-	}
-
-	@Override
-	public double getMaxJoules(Object... data)
-	{
-		return 100;
-	}
-	
-
 }
