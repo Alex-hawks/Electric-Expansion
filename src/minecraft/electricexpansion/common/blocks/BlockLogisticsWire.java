f15b1bdf00c233acb83e19c2f5f4bde51df9fc5b
Mattredsox <mattredsox99@gmail.com>
2012-12-30 21:39:06 -0800

    packet error

--- src/minecraft/electricexpansion/common/blocks/BlockLogisticsWire.java
+++ src/minecraft/electricexpansion/common/blocks/BlockLogisticsWire.java
@@ -1,17 +1,21 @@
 package electricexpansion.common.blocks;
 
+import java.util.EnumSet;
 import java.util.List;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
+import universalelectricity.core.electricity.ElectricityConnections;
+import universalelectricity.core.vector.Vector3;
 import universalelectricity.prefab.BlockConductor;
-import universalelectricity.prefab.implement.IRedstoneProvider;
+import universalelectricity.prefab.network.PacketManager;
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import electricexpansion.common.ElectricExpansion;
@@ -30,6 +34,56 @@ public class BlockLogisticsWire extends BlockConductor
 		this.setBlockBounds(0.30F, 0.30F, 0.30F, 0.70F, 0.70F, 0.70F);
 		this.setRequiresSelfNotify();
 		this.setCreativeTab(EETab.INSTANCE);
+	}
+
+	@Override
+	public void onBlockAdded(World world, int x, int y, int z)
+	{
+		super.onBlockAdded(world, x, y, z);
+		this.updateWireSwitch(world, x, y, z);
+	}
+
+	@Override
+	public void onNeighborBlockChange(World world, int x, int y, int z, int par5)
+	{
+		super.onNeighborBlockChange(world, x, y, z, par5);
+		this.updateWireSwitch(world, x, y, z);
+	}
+
+	private void updateWireSwitch(World world, int x, int y, int z)
+	{
+		TileEntityLogisticsWire tileEntity = (TileEntityLogisticsWire) world.getBlockTileEntity(x, y, z);
+
+		if (!world.isRemote && tileEntity != null)
+		{
+		
+			ElectricityConnections.registerConnector(tileEntity, EnumSet.range(ForgeDirection.DOWN, ForgeDirection.EAST));
+
+
+			for (int i = 0; i < 6; i++)
+			{
+				ForgeDirection direction = ForgeDirection.getOrientation(i);
+
+				Block block = Block.blocksList[world.getBlockId(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ)];
+
+				if (block != null)
+				{
+					if (block.blockID != this.blockID)
+					{
+						try
+						{
+							block.onNeighborBlockChange(world, x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, this.blockID);
+						}
+						catch (Exception e)
+						{
+							FMLLog.severe("Failed to update switch wire");
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+
+		}
 	}
 
 	@Override
@@ -55,19 +109,32 @@ public class BlockLogisticsWire extends BlockConductor
 	{
 		return -1;
 	}
-	
+
+	@Override
+	public TileEntity createNewTileEntity(World var1)
+	{
+		return new TileEntityLogisticsWire();
+	}
+
 	@Override
 	public String getTextureFile()
 	{
 		return ElectricExpansion.ALEX_ITEMS_TEXTURE_FILE;
 	}
 
+	@Override
+	public boolean canProvidePower()
+	{
+		return true;
+	}
+
 	@SideOnly(Side.CLIENT)
 	public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
 	{
-		for (int var4 = 0; var4 < 5; var4++)
+		for (int var4 = 0; var4 < 5; ++var4)
 			par3List.add(new ItemStack(par1, 1, var4));
 	}
+
 	/**
 	 * Called when the block is right clicked by the player
 	 */
@@ -75,61 +142,22 @@ public class BlockLogisticsWire extends BlockConductor
     public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
 	{
 			TileEntityLogisticsWire tileEntity = (TileEntityLogisticsWire) par1World.getBlockTileEntity(par2, par3, par4);
-/*			             if(par1World.isRemote)
-			  	 	
-			              System.out.println(tileEntity.buttonStatus0 + " client");
-			               
-			  	 	
-			              if(!par1World.isRemote)
-			  	 	
-			              System.out.println(tileEntity.buttonStatus0 + " server");*/
-			
-			     for(int x = 0; x < 6; x = x+1) {
-	    	  
+		
 			if(par1World.isRemote)
-				System.out.println(tileEntity.visuallyConnected[x] + " client " + x);
-
-				
-				if(!par1World.isRemote)
-					System.out.println(tileEntity.visuallyConnected[x] + " server " + x);
 			
-			     }	
+  				System.out.println(tileEntity.buttonStatus0 + " client");
+  				
+  				if(!par1World.isRemote)
+  				{
+  					System.out.println(tileEntity.buttonStatus0 + " server");
+  					PacketManager.sendPacketToClients(PacketManager.getPacket(ElectricExpansion.CHANNEL, this, (int) 3, tileEntity.buttonStatus0, tileEntity.buttonStatus1, tileEntity.buttonStatus2), tileEntity.worldObj, new Vector3(tileEntity), 12);
+
+  				}
+  				
 		  	{	
 				par5EntityPlayer.openGui(ElectricExpansion.instance, 3, par1World, par2, par3, par4);
 				return true;
 			}
 			
-	}
-	
-	/**
-	 * Is this block powering the block on the specified side
-	 */
-	@Override
-	public boolean isProvidingStrongPower(IBlockAccess par1IBlockAccess, int x, int y, int z, int side)
-	{
-		TileEntity tileEntity = par1IBlockAccess.getBlockTileEntity(x, y, z);
-
-		if (tileEntity instanceof IRedstoneProvider) { return ((IRedstoneProvider) tileEntity).isPoweringTo(ForgeDirection.getOrientation(side)); }
-
-		return false;
-	}
-
-	/**
-	 * Is this block indirectly powering the block on the specified side
-	 */
-	@Override
-	public boolean isProvidingWeakPower(IBlockAccess par1IBlockAccess, int x, int y, int z, int side)
-	{
-		TileEntity tileEntity = par1IBlockAccess.getBlockTileEntity(x, y, z);
-
-		if (tileEntity instanceof IRedstoneProvider) { return ((IRedstoneProvider) tileEntity).isIndirectlyPoweringTo(ForgeDirection.getOrientation(side)); }
-
-		return false;
-	}
-
-@Override
-	public TileEntity createNewTileEntity(World var1)
-	{
-		return new TileEntityLogisticsWire();
 	}
 }
--- src/minecraft/electricexpansion/common/cables/TileEntityLogisticsWire.java
+++ src/minecraft/electricexpansion/common/cables/TileEntityLogisticsWire.java
@@ -1,86 +1,73 @@
 package electricexpansion.common.cables;
 
-import java.util.EnumSet;
-
 import universalelectricity.core.electricity.Electricity;
 import universalelectricity.core.electricity.ElectricityConnections;
-import universalelectricity.core.electricity.ElectricityPack;
 import universalelectricity.core.implement.IConductor;
-import universalelectricity.core.vector.Vector3;
-import universalelectricity.prefab.implement.IRedstoneProvider;
-import universalelectricity.prefab.network.PacketManager;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.INetworkManager;
-import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.ForgeDirection;
 
 import com.google.common.io.ByteArrayDataInput;
 
-import electricexpansion.common.ElectricExpansion;
 import electricexpansion.common.helpers.TileEntityConductorBase;
 
-public class TileEntityLogisticsWire extends TileEntityConductorBase implements IRedstoneProvider
+public class TileEntityLogisticsWire extends TileEntityConductorBase
 {
 	// everything is in the helper class.
 	// this class MUST remain existent...
-	
+
 	public boolean buttonStatus0 = false;
 	public boolean buttonStatus1 = false;
 	public boolean buttonStatus2 = false;
-		
-	private ElectricityPack oldPack;
-	
-	@Override
-	public void initiate()
-	{
-	//	this.worldObj.notifyBlocksOfNeighborChange(this.xCoord, this.yCoord, this.zCoord, ElectricExpansion.blockLogisticsWire.blockID);
-	//	oldPack = new ElectricityPack();
-		
-		PacketManager.sendPacketToClients(PacketManager.getPacket(ElectricExpansion.CHANNEL, this, (int) 4, this.buttonStatus0, this.buttonStatus1, this.buttonStatus2), this.worldObj, new Vector3(this), 12);
-	}
-	
-	@Override
+
 	public void handlePacketData(INetworkManager network, int type, Packet250CustomPayload packet, EntityPlayer player, ByteArrayDataInput dataStream)
 	{
+		if (this.worldObj.isRemote)
+		{
+			this.visuallyConnected[0] = dataStream.readBoolean();
+			this.visuallyConnected[1] = dataStream.readBoolean();
+			this.visuallyConnected[2] = dataStream.readBoolean();
+			this.visuallyConnected[3] = dataStream.readBoolean();
+			this.visuallyConnected[4] = dataStream.readBoolean();
+			this.visuallyConnected[5] = dataStream.readBoolean();
+			
 			try
 			{
 				int id = dataStream.readInt();
-				if(id == -1) 
+				if (id == 3)
+					{
+						this.buttonStatus0 = dataStream.readBoolean();
+						this.buttonStatus1 = dataStream.readBoolean();
+						this.buttonStatus2 = dataStream.readBoolean();
+					}
+			
+
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+
+		}
+		else
+		{
+			try
+			{
+				int id = dataStream.readInt();
+				if (id == -1)
 				{
-					this.buttonStatus0 = dataStream.readBoolean();			
+					this.buttonStatus0 = dataStream.readBoolean();
 				}
-				if(id == 0)
+				if (id == 0)
 				{
-					this.buttonStatus1 = dataStream.readBoolean();			
+					this.buttonStatus1 = dataStream.readBoolean();
 				}
-				if(id == 1)
+				if (id == 1)
 				{
-					this.buttonStatus2 = dataStream.readBoolean();			
-				}
-				if(id == 3) 
-				{
-						PacketManager.sendPacketToClients(PacketManager.getPacket(ElectricExpansion.CHANNEL, this, (int) 4, this.buttonStatus0, this.buttonStatus1, this.buttonStatus2), this.worldObj, new Vector3(this), 12);
-				}
-				if(id == 4)
-				{
-					this.buttonStatus0 = dataStream.readBoolean();			
-					this.buttonStatus1 = dataStream.readBoolean();			
-					this.buttonStatus2 = dataStream.readBoolean();			
-				}
-				
-				if (this.worldObj.isRemote)
-				{
-					this.visuallyConnected[0] = dataStream.readBoolean();
-					this.visuallyConnected[1] = dataStream.readBoolean();
-					this.visuallyConnected[2] = dataStream.readBoolean();
-					this.visuallyConnected[3] = dataStream.readBoolean();
-					this.visuallyConnected[4] = dataStream.readBoolean();
-					this.visuallyConnected[5] = dataStream.readBoolean();
+					this.buttonStatus2 = dataStream.readBoolean();
 				}
 			}
 			catch (Exception e)
@@ -88,77 +75,83 @@ public class TileEntityLogisticsWire extends TileEntityConductorBase implements 
 				e.printStackTrace();
 			}
 		}
-	
-	@Override
-	public boolean isPoweringTo(ForgeDirection side)
-	{
-		if(this.buttonStatus0 && this.getNetwork().getProduced().getWatts() > 0)
-			return true;
-		
-		return false;
 	}
 
 	@Override
-	public boolean isIndirectlyPoweringTo(ForgeDirection side)
+	public void updateConnection(TileEntity tileEntity, ForgeDirection side)
 	{
-		return isPoweringTo(side);
-	}
-	
-	/**
-	 * Reads a tile entity from NBT.
-	 */
-	@Override
-	public void readFromNBT(NBTTagCompound par1NBTTagCompound)
-	{
-		super.readFromNBT(par1NBTTagCompound);
-		this.buttonStatus0 = par1NBTTagCompound.getBoolean("buttonStatus0");
-		this.buttonStatus1 = par1NBTTagCompound.getBoolean("buttonStatus1");
-		this.buttonStatus2 = par1NBTTagCompound.getBoolean("buttonStatus2");
-
-	}
-
-	/**
-	 * Writes a tile entity to NBT.
-	 */
-	@Override
-	public void writeToNBT(NBTTagCompound par1NBTTagCompound)
-	{
-		super.writeToNBT(par1NBTTagCompound);
-		par1NBTTagCompound.setBoolean("buttonStatus0", this.buttonStatus0);
-		par1NBTTagCompound.setBoolean("buttonStatus1", this.buttonStatus1);
-		par1NBTTagCompound.setBoolean("buttonStatus2", this.buttonStatus2);
-
-	}
-	
-/*	@Override
-	public void updateEntity()
-	{
-		super.updateEntity();
-
-		if (this.ticks % 20 == 0)
+		if (!this.worldObj.isRemote)
 		{
-			if (!this.worldObj.isRemote)
+			if (ElectricityConnections.canConnect(tileEntity, side.getOpposite()))
 			{
+				this.connectedBlocks[side.ordinal()] = tileEntity;
+				this.visuallyConnected[side.ordinal()] = true;
 
-		//System.out.println("TTTTT");
-		ElectricityPack newPack = this.getNetwork().getProduced();
-		
-		if(this.oldPack.getWatts() == 0 && newPack.getWatts() > 0 && this.buttonStatus0)
-		{
-			this.worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, ElectricExpansion.blockLogisticsWire.blockID);
-			System.out.println("Toggling from off to on!");
+				if (tileEntity instanceof IConductor)
+				{
+					Electricity.instance.mergeConnection(this.getNetwork(), ((IConductor) tileEntity).getNetwork());
+				}
+
+				return;
+			}
+
+			if (this.connectedBlocks[side.ordinal()] != null)
+			{
+				if (this.connectedBlocks[side.ordinal()] instanceof IConductor)
+				{
+					Electricity.instance.splitConnection(this, (IConductor) this.getConnectedBlocks()[side.ordinal()]);
+				}
+
+				this.getNetwork().stopProducing(this.connectedBlocks[side.ordinal()]);
+				this.getNetwork().stopRequesting(this.connectedBlocks[side.ordinal()]);
+			}
+
+			this.connectedBlocks[side.ordinal()] = null;
+			this.visuallyConnected[side.ordinal()] = false;
 		}
-		
-		newPack = this.oldPack;
+	}
+
+	@Override
+	public void updateConnectionWithoutSplit(TileEntity tileEntity, ForgeDirection side)
+	{
+		if (!this.worldObj.isRemote)
+		{
+			if (ElectricityConnections.canConnect(tileEntity, side.getOpposite()))
+			{
+				this.connectedBlocks[side.ordinal()] = tileEntity;
+				this.visuallyConnected[side.ordinal()] = true;
+
+				if (tileEntity instanceof IConductor)
+				{
+					Electricity.instance.mergeConnection(this.getNetwork(), ((IConductor) tileEntity).getNetwork());
+				}
+
+				return;
 
 			}
+
+			this.connectedBlocks[side.ordinal()] = null;
+			this.visuallyConnected[side.ordinal()] = false;
 		}
 	}
-*/
+
 	@Override
-	public Packet getDescriptionPacket()
+	public void readFromNBT(NBTTagCompound nbt)
 	{
-		return PacketManager.getPacket(this.channel, this, this.visuallyConnected[0], this.visuallyConnected[1], this.visuallyConnected[2], this.visuallyConnected[3], this.visuallyConnected[4], this.visuallyConnected[5]);
+		super.readFromNBT(nbt);
+		this.buttonStatus0 = nbt.getBoolean("buttonStatus0");
+		this.buttonStatus1 = nbt.getBoolean("buttonStatus1");
+		this.buttonStatus2 = nbt.getBoolean("buttonStatus2");
+
 	}
-	
+
+	@Override
+	public void writeToNBT(NBTTagCompound nbt)
+	{
+		super.writeToNBT(nbt);
+		nbt.setBoolean("buttonStatus0", this.buttonStatus0);
+		nbt.setBoolean("buttonStatus1", this.buttonStatus1);
+		nbt.setBoolean("buttonStatus2", this.buttonStatus2);
+
+	}
 }
