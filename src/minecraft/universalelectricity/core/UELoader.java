7f7967bf07e80c9235f3e51ca724510ac40d86cf
Mattredsox <mattredsox99@gmail.com>
2013-02-03 13:18:38 -0800

    update UE api

--- src/minecraft/universalelectricity/core/UELoader.java
+++ src/minecraft/universalelectricity/core/UELoader.java
@@ -3,8 +3,7 @@ package universalelectricity.core;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.world.ChunkEvent;
-import net.minecraftforge.event.world.WorldEvent.Load;
-import net.minecraftforge.event.world.WorldEvent.Unload;
+import net.minecraftforge.event.world.WorldEvent;
 import universalelectricity.core.electricity.Electricity;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Loader;
@@ -35,24 +34,6 @@ public class UELoader
 			UniversalElectricity.TO_IC2_RATIO = 1 / UniversalElectricity.IC2_RATIO;
 			UniversalElectricity.TO_BC_RATIO = 1 / UniversalElectricity.BC3_RATIO;
 
-			if (UniversalElectricity.BC3_RATIO <= 0 || !Loader.isModLoaded("BuildCraft|Core"))
-			{
-				System.out.println("Disabled Buildcraft electricity conversion!");
-			}
-			else
-			{
-				System.out.println("Buildcraft conversion ratio: " + UniversalElectricity.BC3_RATIO);
-			}
-
-			if (UniversalElectricity.IC2_RATIO <= 0 || !Loader.isModLoaded("IC2"))
-			{
-				System.out.println("Disabled Industrialcraft electricity conversion!");
-			}
-			else
-			{
-				System.out.println("IC2 conversion ratio: " + UniversalElectricity.IC2_RATIO);
-			}
-
 			FMLLog.finest("Universal Electricity v" + UniversalElectricity.VERSION + " successfully loaded!");
 
 			isInitialized = true;
@@ -60,14 +41,14 @@ public class UELoader
 	}
 
 	@ForgeSubscribe
-	public void onWorldUnload(Unload event)
+	public void onWorldUnLoad(WorldEvent.Unload event)
 	{
 		Electricity.instance = new Electricity();
 		Electricity.instance.cleanUpNetworks();
 	}
 
 	@ForgeSubscribe
-	public void onWorldLoad(Load event)
+	public void onWorldLoad(WorldEvent.Load event)
 	{
 		Electricity.instance = new Electricity();
 		Electricity.instance.cleanUpNetworks();
