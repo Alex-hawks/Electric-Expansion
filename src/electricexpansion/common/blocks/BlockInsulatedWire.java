e74c82c64067bab93b1d39c2720863a6e6304538
Henry Mao <admin@calclavia.com>
2013-05-11 12:48:56 +0800

    Fixed some block breaking textures

--- src/electricexpansion/common/blocks/BlockInsulatedWire.java
+++ src/electricexpansion/common/blocks/BlockInsulatedWire.java
@@ -23,182 +23,180 @@ import electricexpansion.common.misc.EETab;
 
 public class BlockInsulatedWire extends BlockConductor
 {
-    public BlockInsulatedWire(int id)
-    {
-        super(id, Material.cloth);
-        this.setUnlocalizedName("InsulatedWire");
-        this.setStepSound(soundClothFootstep);
-        this.setResistance(0.2F);
-        this.setHardness(0.1F);
-        this.setBlockBounds(0.30F, 0.30F, 0.30F, 0.70F, 0.70F, 0.70F);
-        this.setCreativeTab(EETab.INSTANCE);
-    }
-    
-    @Override
-    public boolean isOpaqueCube()
-    {
-        return false;
-    }
-    
-    @Override
-    public boolean renderAsNormalBlock()
-    {
-        return false;
-    }
-    
-    @Override
-    public int damageDropped(int i)
-    {
-        return i;
-    }
-    
-    @Override
-    public int getRenderType()
-    {
-        return -1;
-    }
-    
-    @Override
-    public TileEntity createNewTileEntity(World var1)
-    {
-        return new TileEntityInsulatedWire();
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
-    {
-        for (int var4 = 0; var4 < 5; var4++)
-        {
-            par3List.add(new ItemStack(par1, 1, var4));
-        }
-    }
-    
-    @Override
-    public void onBlockAdded(World world, int x, int y, int z)
-    {
-        super.onBlockAdded(world, x, y, z);
-        
-        TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-        
-        if (tileEntity != null)
-        {
-            if (tileEntity instanceof IConductor)
-            {
-                ((IConductor) tileEntity).updateAdjacentConnections();
-                this.updateWireSwitch(world, x, y, z);
-            }
-        }
-        
-    }
-    
-    /**
-     * Called when the block is right clicked by the player
-     */
-    @Override
-    public boolean onBlockActivated(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int par6,
-            float par7, float par8, float par9)
-    {
-        TileEntityInsulatedWire tileEntity = (TileEntityInsulatedWire) par1World.getBlockTileEntity(x, y, z);
-        
-        if (!par1World.isRemote)
-        {
-            
-            if (par5EntityPlayer.inventory.getCurrentItem() != null)
-            {
-                if (par5EntityPlayer.inventory.getCurrentItem().getItem() instanceof ItemDye)
-                {
-                    
-                    int dyeColor = par5EntityPlayer.inventory.getCurrentItem().getItemDamageForDisplay();
-                    
-                    tileEntity.colorByte = (byte) dyeColor;
-                    
-                    par5EntityPlayer.inventory.getCurrentItem().stackSize = par5EntityPlayer.inventory.getCurrentItem().stackSize - 1;
-                    
-                    PacketManager.sendPacketToClients(PacketManager.getPacket(ElectricExpansion.CHANNEL, tileEntity,
-                            (byte) 0, tileEntity.colorByte));
-                    
-                    ((IConductor) tileEntity).updateAdjacentConnections();
-                    
-                    this.updateWireSwitch(par1World, x, y, z);
-                    
-                    return true;
-                    
-                }
-                
-            }
-            
-        }
-        
-        return false;
-        
-    }
-    
-    private void updateWireSwitch(World world, int x, int y, int z)
-    {
-        TileEntityInsulatedWire tileEntity = (TileEntityInsulatedWire) world.getBlockTileEntity(x, y, z);
-        
-        TileEntity tileEntity1;
-        
-        if (!world.isRemote && tileEntity != null)
-        {
-            
-            for (byte i = 0; i < 6; i++)
-            {
-                switch (i)
-                {
-                    case 0:
-                        tileEntity1 = world.getBlockTileEntity(x + 1, y, z);
-                        break;
-                    case 1:
-                        tileEntity1 = world.getBlockTileEntity(x - 1, y, z);
-                        break;
-                    case 2:
-                        tileEntity1 = world.getBlockTileEntity(x, y + 1, z);
-                        break;
-                    case 3:
-                        tileEntity1 = world.getBlockTileEntity(x, y - 1, z);
-                        break;
-                    case 4:
-                        tileEntity1 = world.getBlockTileEntity(x, y, z + 1);
-                        break;
-                    case 5:
-                        tileEntity1 = world.getBlockTileEntity(x, y, z - 1);
-                        break;
-                    default:
-                        tileEntity1 = world.getBlockTileEntity(x, y, z);
-                }
-                
-                if (tileEntity1 instanceof IConductor)
-                {
-                    ((IConductor) tileEntity1).updateAdjacentConnections();
-                    tileEntity1.worldObj.markBlockForUpdate(tileEntity1.xCoord, tileEntity1.yCoord, tileEntity1.zCoord);
-                }
-            }
-        }
-    }
-    
-    @Override
-    public void setBlockBoundsBasedOnState(IBlockAccess par1IBlockAccess, int x, int y, int z)
-    {
-        TileEntity tileEntity = par1IBlockAccess.getBlockTileEntity(x, y, z);
-        if (tileEntity instanceof TileEntityConductorBase)
-        {
-            TileEntityConductorBase te = (TileEntityConductorBase) tileEntity;
-            this.minX = te.connectedBlocks[4] != null ? 0F : 0.3F;
-            this.minY = te.connectedBlocks[0] != null ? 0F : 0.3F;
-            this.minZ = te.connectedBlocks[2] != null ? 0F : 0.3F;
-            this.maxX = te.connectedBlocks[5] != null ? 1F : 0.7F;
-            this.maxY = te.connectedBlocks[1] != null ? 1F : 0.7F;
-            this.maxZ = te.connectedBlocks[3] != null ? 1F : 0.7F;
-        }
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
-    {
-    }
-    
+	public BlockInsulatedWire(int id)
+	{
+		super(id, Material.cloth);
+		this.setUnlocalizedName("InsulatedWire");
+		this.setStepSound(soundClothFootstep);
+		this.setResistance(0.2F);
+		this.setHardness(0.1F);
+		this.setBlockBounds(0.30F, 0.30F, 0.30F, 0.70F, 0.70F, 0.70F);
+		this.setCreativeTab(EETab.INSTANCE);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerIcons(IconRegister par1IconRegister)
+	{
+		this.blockIcon = par1IconRegister.registerIcon(ElectricExpansion.TEXTURE_NAME_PREFIX + "InsulatedWire.Copper");
+	}
+
+	@Override
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean renderAsNormalBlock()
+	{
+		return false;
+	}
+
+	@Override
+	public int damageDropped(int i)
+	{
+		return i;
+	}
+
+	@Override
+	public int getRenderType()
+	{
+		return -1;
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World var1)
+	{
+		return new TileEntityInsulatedWire();
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
+	{
+		for (int var4 = 0; var4 < 5; var4++)
+		{
+			par3List.add(new ItemStack(par1, 1, var4));
+		}
+	}
+
+	@Override
+	public void onBlockAdded(World world, int x, int y, int z)
+	{
+		super.onBlockAdded(world, x, y, z);
+
+		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+
+		if (tileEntity != null)
+		{
+			if (tileEntity instanceof IConductor)
+			{
+				((IConductor) tileEntity).updateAdjacentConnections();
+				this.updateWireSwitch(world, x, y, z);
+			}
+		}
+
+	}
+
+	/**
+	 * Called when the block is right clicked by the player
+	 */
+	@Override
+	public boolean onBlockActivated(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+	{
+		TileEntityInsulatedWire tileEntity = (TileEntityInsulatedWire) par1World.getBlockTileEntity(x, y, z);
+
+		if (!par1World.isRemote)
+		{
+
+			if (par5EntityPlayer.inventory.getCurrentItem() != null)
+			{
+				if (par5EntityPlayer.inventory.getCurrentItem().getItem() instanceof ItemDye)
+				{
+
+					int dyeColor = par5EntityPlayer.inventory.getCurrentItem().getItemDamageForDisplay();
+
+					tileEntity.colorByte = (byte) dyeColor;
+
+					par5EntityPlayer.inventory.getCurrentItem().stackSize = par5EntityPlayer.inventory.getCurrentItem().stackSize - 1;
+
+					PacketManager.sendPacketToClients(PacketManager.getPacket(ElectricExpansion.CHANNEL, tileEntity, (byte) 0, tileEntity.colorByte));
+
+					((IConductor) tileEntity).updateAdjacentConnections();
+
+					this.updateWireSwitch(par1World, x, y, z);
+
+					return true;
+
+				}
+
+			}
+
+		}
+
+		return false;
+
+	}
+
+	private void updateWireSwitch(World world, int x, int y, int z)
+	{
+		TileEntityInsulatedWire tileEntity = (TileEntityInsulatedWire) world.getBlockTileEntity(x, y, z);
+
+		TileEntity tileEntity1;
+
+		if (!world.isRemote && tileEntity != null)
+		{
+
+			for (byte i = 0; i < 6; i++)
+			{
+				switch (i)
+				{
+					case 0:
+						tileEntity1 = world.getBlockTileEntity(x + 1, y, z);
+						break;
+					case 1:
+						tileEntity1 = world.getBlockTileEntity(x - 1, y, z);
+						break;
+					case 2:
+						tileEntity1 = world.getBlockTileEntity(x, y + 1, z);
+						break;
+					case 3:
+						tileEntity1 = world.getBlockTileEntity(x, y - 1, z);
+						break;
+					case 4:
+						tileEntity1 = world.getBlockTileEntity(x, y, z + 1);
+						break;
+					case 5:
+						tileEntity1 = world.getBlockTileEntity(x, y, z - 1);
+						break;
+					default:
+						tileEntity1 = world.getBlockTileEntity(x, y, z);
+				}
+
+				if (tileEntity1 instanceof IConductor)
+				{
+					((IConductor) tileEntity1).updateAdjacentConnections();
+					tileEntity1.worldObj.markBlockForUpdate(tileEntity1.xCoord, tileEntity1.yCoord, tileEntity1.zCoord);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess par1IBlockAccess, int x, int y, int z)
+	{
+		TileEntity tileEntity = par1IBlockAccess.getBlockTileEntity(x, y, z);
+		if (tileEntity instanceof TileEntityConductorBase)
+		{
+			TileEntityConductorBase te = (TileEntityConductorBase) tileEntity;
+			this.minX = te.connectedBlocks[4] != null ? 0F : 0.3F;
+			this.minY = te.connectedBlocks[0] != null ? 0F : 0.3F;
+			this.minZ = te.connectedBlocks[2] != null ? 0F : 0.3F;
+			this.maxX = te.connectedBlocks[5] != null ? 1F : 0.7F;
+			this.maxY = te.connectedBlocks[1] != null ? 1F : 0.7F;
+			this.maxZ = te.connectedBlocks[3] != null ? 1F : 0.7F;
+		}
+	}
 }
--- src/electricexpansion/common/blocks/BlockTransformer.java
+++ src/electricexpansion/common/blocks/BlockTransformer.java
@@ -19,169 +19,168 @@ import universalelectricity.prefab.tile.TileEntityAdvanced;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import electricexpansion.client.ClientProxy;
+import electricexpansion.common.ElectricExpansion;
 import electricexpansion.common.misc.EETab;
 import electricexpansion.common.tile.TileEntityTransformer;
 
 public class BlockTransformer extends BlockAdvanced
 {
-    public BlockTransformer(int id)
-    {
-        super(id, UniversalElectricity.machine);
-        this.setStepSound(soundMetalFootstep);
-        this.setCreativeTab(EETab.INSTANCE);
-        this.setUnlocalizedName("transformer");
-    }
-    
-    /**
-     * Called when the block is placed in the world.
-     */
-    @Override
-    public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLiving par5EntityLiving, ItemStack itemStack)
-    {
-        int metadata = par1World.getBlockMetadata(x, y, z);
-        int tierStart = metadata - (metadata & 3);
-        
-        int angle = MathHelper.floor_double(par5EntityLiving.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
-        switch (angle)
-        {
-            case 0:
-                par1World.setBlock(x, y, z, this.blockID, tierStart + 3, 0);
-                break;
-            case 1:
-                par1World.setBlock(x, y, z, this.blockID, tierStart + 1, 0);
-                break;
-            case 2:
-                par1World.setBlock(x, y, z, this.blockID, tierStart + 2, 0);
-                break;
-            case 3:
-                par1World.setBlock(x, y, z, this.blockID, tierStart + 0, 0);
-                break;
-        }
-        
-        ((TileEntityAdvanced) par1World.getBlockTileEntity(x, y, z)).initiate();
-        par1World.notifyBlocksOfNeighborChange(x, y, z, this.blockID);
-    }
-    
-    @Override
-    public boolean onUseWrench(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int side,
-            float hitX, float hitY, float hitZ)
-    {
-        int metadata = par1World.getBlockMetadata(x, y, z);
-        int tierStart = metadata - (metadata & 3);
-        int original = metadata & 3;
-        
-        int change = 0;
-        
-        // Re-orient the block
-        switch (original)
-        {
-            case 0:
-                change = 3;
-                break;
-            case 3:
-                change = 1;
-                break;
-            case 1:
-                change = 2;
-                break;
-            case 2:
-                change = 0;
-                break;
-        }
-        
-        par1World.setBlock(x, y, z, this.blockID, change + tierStart, 0);
-        par1World.markBlockForRenderUpdate(x, y, z);
-        
-        ((TileEntityAdvanced) par1World.getBlockTileEntity(x, y, z)).initiate();
-        
-        return true;
-    }
-    
-    @Override
-    public boolean onSneakUseWrench(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int side,
-            float hitX, float hitY, float hitZ)
-    {
-        if (!par1World.isRemote)
-        {
-            TileEntityTransformer tileEntity = (TileEntityTransformer) par1World.getBlockTileEntity(x, y, z);
-            
-            tileEntity.stepUp = !tileEntity.stepUp;
-            return true;
-        }
-        
-        return false;
-    }
-    
-    @Override
-    public boolean isOpaqueCube()
-    {
-        return false;
-    }
-    
-    @Override
-    public boolean isBlockSolidOnSide(World world, int x, int y, int z, ForgeDirection side)
-    {
-        return side.equals(ForgeDirection.DOWN);
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public int getRenderType()
-    {
-        return ClientProxy.RENDER_ID;
-    }
-    
-    @Override
-    public boolean renderAsNormalBlock()
-    {
-        return false;
-    }
-    
-    @Override
-    public TileEntity createTileEntity(World var1, int metadata)
-    {
-        return new TileEntityTransformer();
-    }
-    
-    @Override
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
-    {
-        for (int i = 0; i < 9; i += 4)
-        {
-            par3List.add(new ItemStack(this, 1, i));
-        }
-    }
-    
-    @Override
-    public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z)
-    {
-        int id = this.idPicked(world, x, y, z);
-        
-        if (id == 0)
-            return null;
-        
-        Item item = Item.itemsList[id];
-        if (item == null)
-            return null;
-        
-        int metadata = world.getBlockMetadata(x, y, z);
-        int tierStart = metadata - (metadata & 3);
-        
-        return new ItemStack(id, 1, tierStart);
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
-    {
-        
-    }
-    
-    @Override
-    public int damageDropped(int metadata)
-    {
-        return metadata - (metadata & 3);
-    }
-    
+	public BlockTransformer(int id)
+	{
+		super(id, UniversalElectricity.machine);
+		this.setStepSound(soundMetalFootstep);
+		this.setCreativeTab(EETab.INSTANCE);
+		this.setUnlocalizedName("transformer");
+	}
+
+	/**
+	 * Called when the block is placed in the world.
+	 */
+	@Override
+	public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLiving par5EntityLiving, ItemStack itemStack)
+	{
+		int metadata = par1World.getBlockMetadata(x, y, z);
+		int tierStart = metadata - (metadata & 3);
+
+		int angle = MathHelper.floor_double(par5EntityLiving.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
+		switch (angle)
+		{
+			case 0:
+				par1World.setBlock(x, y, z, this.blockID, tierStart + 3, 0);
+				break;
+			case 1:
+				par1World.setBlock(x, y, z, this.blockID, tierStart + 1, 0);
+				break;
+			case 2:
+				par1World.setBlock(x, y, z, this.blockID, tierStart + 2, 0);
+				break;
+			case 3:
+				par1World.setBlock(x, y, z, this.blockID, tierStart + 0, 0);
+				break;
+		}
+
+		((TileEntityAdvanced) par1World.getBlockTileEntity(x, y, z)).initiate();
+		par1World.notifyBlocksOfNeighborChange(x, y, z, this.blockID);
+	}
+
+	@Override
+	public boolean onUseWrench(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int side, float hitX, float hitY, float hitZ)
+	{
+		int metadata = par1World.getBlockMetadata(x, y, z);
+		int tierStart = metadata - (metadata & 3);
+		int original = metadata & 3;
+
+		int change = 0;
+
+		// Re-orient the block
+		switch (original)
+		{
+			case 0:
+				change = 3;
+				break;
+			case 3:
+				change = 1;
+				break;
+			case 1:
+				change = 2;
+				break;
+			case 2:
+				change = 0;
+				break;
+		}
+
+		par1World.setBlock(x, y, z, this.blockID, change + tierStart, 0);
+		par1World.markBlockForRenderUpdate(x, y, z);
+
+		((TileEntityAdvanced) par1World.getBlockTileEntity(x, y, z)).initiate();
+
+		return true;
+	}
+
+	@Override
+	public boolean onSneakUseWrench(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int side, float hitX, float hitY, float hitZ)
+	{
+		if (!par1World.isRemote)
+		{
+			TileEntityTransformer tileEntity = (TileEntityTransformer) par1World.getBlockTileEntity(x, y, z);
+
+			tileEntity.stepUp = !tileEntity.stepUp;
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isBlockSolidOnSide(World world, int x, int y, int z, ForgeDirection side)
+	{
+		return side.equals(ForgeDirection.DOWN);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public int getRenderType()
+	{
+		return ClientProxy.RENDER_ID;
+	}
+
+	@Override
+	public boolean renderAsNormalBlock()
+	{
+		return false;
+	}
+
+	@Override
+	public TileEntity createTileEntity(World var1, int metadata)
+	{
+		return new TileEntityTransformer();
+	}
+
+	@Override
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
+	{
+		for (int i = 0; i < 9; i += 4)
+		{
+			par3List.add(new ItemStack(this, 1, i));
+		}
+	}
+
+	@Override
+	public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z)
+	{
+		int id = this.idPicked(world, x, y, z);
+
+		if (id == 0)
+			return null;
+
+		Item item = Item.itemsList[id];
+		if (item == null)
+			return null;
+
+		int metadata = world.getBlockMetadata(x, y, z);
+		int tierStart = metadata - (metadata & 3);
+
+		return new ItemStack(id, 1, tierStart);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister par1IconRegister)
+	{
+		this.blockIcon = par1IconRegister.registerIcon(ElectricExpansion.TEXTURE_NAME_PREFIX + "darkMachine");
+	}
+
+	@Override
+	public int damageDropped(int metadata)
+	{
+		return metadata - (metadata & 3);
+	}
+
 }
